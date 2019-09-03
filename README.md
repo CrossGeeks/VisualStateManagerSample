9f2cd244e211e45c01d1f37d6bc469e51f828960
Charlin Agramonte <char0394@gmail.com>
2019-09-03 10:12:14 -0400

    Adding code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# VisualStateManagerSample
+# Visual State Manager Sample
+
+<p align="center">
+<img src="sample.gif?raw=true"  title="iOS"/>
+</p>
---
+++ VisualStateManagerSample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace VisualStateManagerSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Label lastElementSelected;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            for(int i=1; i<DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month); i++)
+            {
+                mainLayout.Children.Add(new Label() {Text=$"{i}", TextColor=Color.White, VerticalOptions=LayoutOptions.Center});
+            }
+
+            foreach (var item in mainLayout.Children)
+            {
+                var tapGestureRecognizer = new TapGestureRecognizer();
+                tapGestureRecognizer.Tapped += Butto_Clicked;
+                item.GestureRecognizers.Add(tapGestureRecognizer);
+            }
+        }
+
+        private void Butto_Clicked(object sender, EventArgs e)
+        {
+            if (lastElementSelected != null)
+                VisualStateManager.GoToState(lastElementSelected, "UnSelected");
+
+
+            VisualStateManager.GoToState((Label)sender, "Selected");
+
+            lastElementSelected = (Label)sender;
+        }
+    }
+}
